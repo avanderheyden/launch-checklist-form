d374018ff36341b5c71893a1edb10482ae9c23d1
Amanda Vanderheyden <aev4882@gmail.com>
2019-10-16 19:19:08 -0500

    query-selector

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 // Write your JavaScript code here!
+window.addEventListener('load', function () {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function (event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoWeight = document.querySelector("input[name=cargoWeight]");
+      if (pilotName.value === " " || copilotName.value === " " || fuelLevel.value === " " || cargoWeight.value === " ") {
+         alert("All fields are required!");
+         event.preventDefault();
+      }
+      // this.document.getElementById("launchStatusCheck").onclick = function () {
+      let cargoStatus = document.getElementById("cargoStatus");
+      document.getElementById("cargoStatus").innerHTML = "Too much mass for shuttle to take off";
+
+      let launchStatus = document.getElementById("launchStatus");
+      document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+
+      let textColor = document.querySelector("launchStatus");
+      document.querySelector("launchStatus").style.color = "red";
+
+      // }
+   });
+});
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
